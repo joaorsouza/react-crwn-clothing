dc22b1d89fd15b439e9adb6bf73fa42eb7ac2469
João Rafael de Souza <joaorsouza@yahoo.com.br>
2022-01-16 09:42:55 -0300

    Add css style to menu-item

--- src/components/menu-item/Menu-item.component.jsx
+++ src/components/menu-item/Menu-item.component.jsx
@@ -1,19 +1,22 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import './Menu-item.scss'
-import { withRouter } from 'react-router-dom'
+import React from "react";
+import "./Menu-item.scss";
+import { withRouter } from "react-router-dom";
 
 function MenuItem({ title, imageUrl, size, history, linkUrl, match }) {
-  
   return (
-    
-    <div className={`${size} menu-item`} style={{ 
-      backgroundImage: `url(${imageUrl})`
-      }}
-      onClick={() => {history.push(`${match.url}${linkUrl}`)}}
-      >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+        onClick={() => {
+          history.push(`${match.url}${linkUrl}`);
+        }}
+      />
       <div className="content">
-        <h1 className="title">{title || ''}</h1>
+        <h1 className="title">{title.toUpperCase() || ""}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
