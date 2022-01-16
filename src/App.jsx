ff077ea402989149b1912aa436f9f0363bf6bafc
João Rafael de Souza <joaorsouza@yahoo.com.br>
2022-01-16 12:58:13 -0300

    Add Header component and router switch tag

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
