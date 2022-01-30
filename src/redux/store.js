8072e6f821d09fa47cfeba203e11f401638c53aa
João Rafael de Souza <joaorsouza@yahoo.com.br>
2022-01-30 08:00:26 -0300

    Add redux and implement with userUreducer on header and app components

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import rootReducer from "./root.reducer";
+
+const middleswares =[logger]
+
+const store = createStore(rootReducer, applyMiddleware(...middleswares))
+
+export default store
