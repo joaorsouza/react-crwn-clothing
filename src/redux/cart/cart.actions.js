652e9ba4fafec6da30610ce86aa9928104c9b5ac
João Rafael de Souza <joaorsouza@yahoo.com.br>
2022-02-13 23:33:17 -0300

    Add remove item quantity feature to checkout item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -12,6 +12,11 @@ export const addItem = item => ({
     payload: item
 })
 
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+})
+
 export const clearItemFomCart = item => ({
     type: CartActionTypes.CLEAR_ITEM_FROM_CART,
     payload: item
